38709027a1b4bf2b211bc2b60bbd1c4beec37429
Victor Stone <victor.stone@gmail.com>
2016-09-11 20:07:28 -0700

    codedeploy (18)

--- stage/after-install.sh
+++ stage/after-install.sh
@@ -1,10 +1,7 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/concierge
-mkdir node_modules || true
-chmod 777 node_modules
 npm install
 npm install gulp -g
 npm install pm2 -g
 pm2 update
 gulp no-watch
-chmod 750 node_modules
