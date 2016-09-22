eb12e73ff5c1025251e116b2b633258fe1a31863
Alex Melman <melmanalex@gmail.com>
2016-09-21 22:37:57 -0500

    More homepage edits

--- src/client/main/components/HomePage.jsx
+++ src/client/main/components/HomePage.jsx
@@ -38,13 +38,13 @@ class HomePage extends PageContext(React.Component) {
               <div className="pledge-area row">
                 <div className="pledge-col col s12 m4">
                   <div className="pledge">
-                    <Link className="pledge-button btn waves-effect waves-light" to="https://secure.actblue.com/contribute/page/mvmt-us-movement2016-c4?refcode=homepage">Easy</Link>
+                    <Link className="pledge-button btn waves-effect waves-light" to="/donate">Easy</Link>
                     <div className="pledge-desc">Split your contribution evenly between all Movement 2016 groups in purple states.</div>
                   </div>
                 </div>
                 <div className="pledge-col col s12 m4">
                   <div className="pledge">
-                    <Link className="pledge-button btn waves-effect waves-light" to="https://movement2016.org/contribution-planning">Customized</Link>
+                    <Link className="pledge-button btn waves-effect waves-light" to="/groups">Customized</Link>
                     <div className="pledge-desc">Custom plan your giving. Filter groups by state, issue area, or nonprofit tax status.</div>
                   </div>
                 </div>
@@ -59,6 +59,32 @@ class HomePage extends PageContext(React.Component) {
           </div>
         </section>
         <section className="testimonial-section">
+          <div className="row">
+            <div className="col s12 m6 l3">
+              <div className="testimonial">
+                <div className="testimonial-content">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div>
+                <div className="testimonial-author">- Steve Baliwag</div>
+              </div>
+            </div>
+          <div className="col s12 m6 l3">
+              <div className="testimonial">
+                <div className="testimonial-content">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div>
+                <div className="testimonial-author">- Steve Baliwag</div>
+              </div>
+            </div>
+          <div className="col s12 m6 l3">
+              <div className="testimonial">
+                <div className="testimonial-content">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div>
+                <div className="testimonial-author">- Steve Baliwag</div>
+              </div>
+            </div>
+          <div className="col s12 m6 l3">
+              <div className="testimonial">
+                <div className="testimonial-content">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div>
+                <div className="testimonial-author">- Steve Baliwag</div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="volunteer-section">
         </section>
