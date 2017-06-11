3e73efd5388d5f89b60c2721a6c06db93717019c
Victor Stone <victor.stone@gmail.com>
2017-06-10 19:59:14 -0700

    new branch for mobile feature test

--- src/shared/m-service/index.js
+++ src/shared/m-service/index.js
@@ -5,12 +5,12 @@ var path    = require('jspath');
 
 let _fetch = null;
 
-if( typeof window !== 'undefined') {
+if( global.IS_SERVER_REQUEST ) {
+  _fetch = require('node-fetch'.trim()); // prevent browserify bundling  
+} else {
   _fetch = (url) => {
     return new Promise( (success,error) => $.ajax({url,success,error}) ); //  ,xhrFields: {withCredentials:false}} ) );
   };
-} else {
-  _fetch = require('node-fetch'.trim()); // prevent browserify bundling
 }
 
 const WP_DEV      = false;
@@ -45,7 +45,6 @@ class MovementVoteService {
   }
 
   init( ) {
-    console.log('service-init');
     return this;
   }
 
@@ -58,45 +57,42 @@ class MovementVoteService {
       : (this._promises['content'] = this._fetch( 'content' )).then( p => {this._promises['content'] = null; return this._content = p;} );
   }
 
-  getPage(slug) {
-    return this._pages[slug]
-      ? Promise.resolve(this._pages[slug])
-      : this._fetch( 'page/' + slug ).then( p => this._pages[slug] = p );
-  }
-
-/* Non-promise */
-
-  cachedPage(slug) {
-    return this._pages[slug] || null;
-  }
-
   get news() {
-    return this._content.posts.news;
+    return this._news
+      ? Promise.resolve(this._news)
+      : this.content.then( () => this._news = this._content.posts.news );
   }
 
   get donateTiles() {
-    return this._content.posts.donatetile;
+    return this._donateTiles
+      ? Promise.resolve(this._donateTiles)
+      : this.content.then( () => this._donateTiles = this._content.posts.donatetile );
   }
 
   get testimonials() {
-    return this._content.posts.testimonial;
+    return this._testimonials
+      ? Promise.resolve(this._testimonials)
+      : this.content.then( () => this._testimonials = this._content.posts.testimonial );
   }
 
   get advisors() {
-    return this._content.posts.advisor;
+    return this._advisors
+      ? Promise.resolve(this._advisors)
+      : this.content.then( () => this._advisors = this._content.posts.advisor );
   }
 
   get groups() {
-    return this._content.posts.group;
-  }
-
-  get colorOrder() {
-    return this._content.colorOrder;
+    return this._groups
+      ? Promise.resolve(this._groups)
+      : this.content.then( () => this._groups = this._content.posts.group );
   }
 
   /* Returns a structured array of groups with structure orgs[color][state][org] */
   get orgs() {
-    if( !this._orgs ) {
+    if( this._orgs ) {
+      return Promise.resolve(this._orgs);
+    }
+    return this.content.then( () => {
       const orgs = {};
       const colors = this.colorSections;
       colors.forEach( color => {
@@ -107,69 +103,104 @@ class MovementVoteService {
           foundOrgs.length && (orgs[color.slug][state.slug] = foundOrgs);
         });
       });
-      this._orgs = orgs;
-    }
-    return this._orgs;
+      return this._orgs = orgs;
+    });
   }
 
   /* Returns a structured object of menu items with structure menu.parentItem.childItem */
   get menu() {
-    if ( !this._menu ) {
-      var menu = {};
-      this._content.menu.forEach( item => {
-        const parent = parseInt(item.parent);
-        var id = item.ID;
-        if( parent === 0 ) {
-          if( !menu[id] ) {
-            menu[id] = item;
-            menu[id].children = [];
-          }
-        } else {
-          if( !menu[parent] ) {
-            var parentItem = path('.menu{.ID=='+parent+'}',this._content)[0];
-            menu[parent] = parentItem;
-            menu[parent].children = [];
-          }
-          menu[parent].children.push(item);
-        }
-      });
-      this._menu = Object.keys(menu).map( k => menu[k]);
-    }
-    return this._menu;
+    return this._menu
+      ? Promise.resolve(this._menu)
+      : this.content.then( () => {
+            var menu = {};
+            this._content.menu.forEach( item => {
+              const parent = parseInt(item.parent);
+              var id = item.ID;
+              if( parent === 0 ) {
+                if( !menu[id] ) {
+                  menu[id] = item;
+                  menu[id].children = [];
+                }
+              } else {
+                if( !menu[parent] ) {
+                  var parentItem = path('.menu{.ID=='+parent+'}',this._content)[0];
+                  menu[parent] = parentItem;
+                  menu[parent].children = [];
+                }
+                menu[parent].children.push(item);
+              }
+            });
+            this._menu = Object.keys(menu).map( k => menu[k]);
+            return this._menu;
+        });
+  }
+
+  getPage(slug) {
+    return this._pages[slug]
+      ? Promise.resolve(this._pages[slug])
+      : this._fetch( 'page/' + slug ).then( p => this._pages[slug] = p );
   }
 
   /* Returns an unsorted list of states */
   get states() {
-    if ( !this._states ) {
-      this._states = path('.taxonomies.state.terms.*{.parent!=0}', this._content);
-    }
-    return this._states;
+    return this.content.then( () => this._states = this.statesList );
   }
 
-  /* Returns a list of state color categories sorted in correct display order */
-  get colorSections() {
-    if ( !this._colorSections ) {
-      var colors = path('.taxonomies.state.terms.*{.parent==0}', this._content);
-      var orderMap = {};
-      this.colorOrder.forEach( (c,i) => orderMap[c] = i );
-      this._colorSections = colors.sort( (a,b) => orderMap[a.slug] > orderMap[b.slug] );
-    }
-    return this._colorSections;
+  /* Returns an unsorted list of state color categories */
+  get stateColors() {
+    return this.content.then( () => this._stateColors = this.statesInColor(0) );
+  }
+
+  get stateProps() {
+    return this._stateProps
+      ? Promise.resolve(this._stateProps)
+      : Promise.all( [ this.states, this.stateColors ] )
+               .then( ([states,stateColors]) => this._stateProps = { stateColors, states } ) ;
+  }
+
+  get filters() {
+    return this.content.then( () => this.groupFilters );
+  }
+
+
+/* NON PROMISE */
+
+  cachedValue(key) {
+    let prop = '_' + key;
+    return this[prop] || null;
+  }
+
+  cachedPage(slug) {
+    return this._pages[slug] || null;
+  }
+
+
+  /* Returns a list of states */
+  get statesList() {
+    return path('.taxonomies.state.terms.*{.parent!=0}', this._content);
   }
 
   /* Returns total group number */
   get numGroups() {
     if ( !this._numGroups ) {
-      this._numGroups = Object.keys(this.groups).length;
+      this._numGroups = Object.keys(this._content.posts.group).length;
     }
     return this._numGroups;
   }
 
+  /* Returns a list of state color categories sorted in correct display order */
+  get colorSections() {
+    var colors = this.statesInColor(0);
+    var orderMap = {};
+    this.colorOrder.forEach( (c,i) => orderMap[c] = i );
+    return colors.sort( (a,b) => orderMap[a.slug] > orderMap[b.slug] );
+  }
+
   /* Returns a dictionary of states */
   get statesDict() {
-    if ( !this._statesDict ) {
+    if( !this._statesDict ) {
       const statesList = {};
-      this.states.forEach( g => statesList[g.slug] = g );
+      this.statesList.forEach( g => statesList[g.slug] = g );
       this._statesDict = statesList;
     }
     return this._statesDict;
@@ -177,7 +208,7 @@ class MovementVoteService {
 
   /* Returns a dictionary of color sections */
   get colorSectionsDict() {
-    if ( !this._colorSectionsDict ) {
+    if( !this._colorSectionsDict ) {
       const dict = {};
       this.colorSections.forEach( c => {
         dict[c.slug] = c;
@@ -189,43 +220,18 @@ class MovementVoteService {
   }
 
   get colorSectionsIDDict() {
-    if ( !this._colorSectionsIDDict ) {
-      this._colorSectionsIDDict = this.colorSections.reduce(
-        (dict,color) => (dict[color.term_id] = color, dict), {}
-      );
-    }
-    return this._colorSectionsIDDict;
+    return this._colorSectionsIDDict || (this._colorSectionsIDDict = this.statesInColor(0).reduce( (dict,color) => (dict[color.term_id] = color, dict), {} ));
   }
 
-  get groupFilters() {
-    if( !this._groupFilters ) {
-      const filters = {};
-      // remove 'state' from taxonomies
-      const taxonomies = this._content.taxonomies;
-
-      for (var t in taxonomies) {
-        if ( !( t === 'state' /* || t === 'nonprofit-type' */ ) ) {
-          filters[t] = taxonomies[t];
-          filters[t].tags = Object.keys(filters[t].terms);
-        }
-      }
-      this._groupFilters = filters;
-    }
-    return this._groupFilters;
+  get colorOrder() {
+    return this._content.colorOrder;
   }
 
-  get filterDict() {
-    if( !this._filterDict ) {
-      this._filterDict = {};
-      path('...terms.*', this.groupFilters ).map( f => this._filterDict[f.name] = f.label );
-    }
-    return this._filterDict;
-  }
-
-  /* Returns a list of states when passed a color category object. */
+  /* Returns a list of states when passed a color category object.
+     If no color category is passed, returns an unsorted list of color categories. */
   statesInColor(color) {
-    var id = color['term_id'];
-    return path('.taxonomies.state.terms.*{.parent=='+id+'}', this._content);
+    var id = (color && color['term_id']) || 0;
+    return path('.taxonomies.state.terms.*{.parent=='+id+'}',this._content);
   }
 
   /* Returns number of groups in the passed color category */
@@ -234,6 +240,28 @@ class MovementVoteService {
     const states = this.statesInColor(color);
     states.forEach( s => total += s.count );
     return total;
+  }
+
+  get groupFilters() {
+    if( !this._groupFilters ) {
+      const filters = {};
+      // remove 'state' from taxonomies
+      const tax = this._content.taxonomies;
+      Object.keys( tax ).filter( k => k !== 'state').forEach( k => {
+          filters[k] = tax[k];
+          filters[k].tags = Object.keys(filters[k].terms);
+        });
+      this._groupFilters = filters;
+    }
+    return this._groupFilters;
+  }
+
+  get filterDict() {
+    if( !this._filterDict ) {
+      this._filterDict = {};
+      path('...terms.*', this.groupFilters ).forEach( f => this._filterDict[f.name] = f.label );
+    }
+    return this._filterDict;
   }
 
 }
