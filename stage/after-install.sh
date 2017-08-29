c79be0eb993446268dd650260da3fd3b56194a4a
Victor Stone <victor.stone@gmail.com>
2017-08-28 18:47:08 -0700

    ec2 -> s3

--- stage/after-install.sh
+++
@@ -1,8 +0,0 @@
-source $HOME/.bash_profile
-cd $HOME/concierge
-npm install
-npm rebuild node-sass
-npm install gulp -g
-npm install pm2@latest -g
-pm2 update
-gulp no-watch
--- stage/app-start.sh
+++
@@ -1,4 +0,0 @@
-source $HOME/.bash_profile
-cd $HOME/concierge
-pm2 delete all || true
-pm2 start -n mainapp $HOME/concierge/dist/server/main.js
