ab75b7fddc70f797d3b1980ca476e07fe07e2b81
Alex Melman <melmanalex@gmail.com>
2018-06-13 06:57:46 -0400

    about page styles

--- src/client/components/AboutPage.jsx
+++ src/client/components/AboutPage.jsx
@@ -7,45 +7,39 @@ import { idFormat } from '../lib/helperFunctions';
 /* eslint-disable react/no-danger */
 
 const PageMenuItem = ({ title }) => (
-	<a onClick={() => scrollToElement('#' + idFormat(title))}>{title}</a>
+  <a onClick={() => scrollToElement('#' + idFormat(title))}>{title}</a>
 );
 
 const PageSection = ({ title, content, image }) => (
-	<section id={idFormat(title)} className="page-section">
-		<div className="container small-container">
-			<h2 className="section-title">{title}</h2>
-			<div
-				className="section-body"
-				dangerouslySetInnerHTML={{ __html: content }}
-			/>
-		</div>
-		<div
-			className="image-banner"
-			style={{ backgroundImage: `url(${image})` }}
-		/>
-	</section>
+  <section id={idFormat(title)} className="page-section">
+    <div className="container small-container">
+      <h2 className="section-title">{title}</h2>
+      <div className="section-body" dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+    <div className="image-banner" style={{ backgroundImage: `url(${image})` }} />
+  </section>
 );
 
 const _AboutPage = ({ title, headerImage, pageSections }) => (
-	<main className="about-page">
-		<div className="page-header" style={{ backgroundImage: `url(${headerImage})` }}>
-			<h1 className="page-title">{title}</h1>
-			<nav className="page-menu">
-				{pageSections.map((d, i) => <PageMenuItem key={i} title={d.title} />)}
-			</nav>
-		</div>
-		{pageSections.map((d, i) => <PageSection key={i} {...d} />)}
-	</main>
+  <main className="about-page">
+    <div className="page-header" style={{ backgroundImage: `url(${headerImage})` }}>
+      <h1 className="page-title">{title}</h1>
+      <div className="page-menu">
+        {pageSections.map((d, i) => <PageMenuItem key={i} title={d.title} />)}
+      </div>
+    </div>
+    {pageSections.map((d, i) => <PageSection key={i} {...d} />)}
+  </main>
 );
 
 const mapStateToProps = ({
-	router: {
-		target: {
-			model: {
-				page: { title, aboutPageSections: pageSections, image: headerImage },
-			},
-		},
-	},
+  router: {
+    target: {
+      model: {
+        page: { title, aboutPageSections: pageSections, image: headerImage },
+      },
+    },
+  },
 }) => ({ title, headerImage, pageSections });
 
 const AboutPage = connect(mapStateToProps)(_AboutPage);
