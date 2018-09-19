2901d3f74395ba15e2e801965817f30cb2408f21
Alex Melman <melmanalex@gmail.com>
2018-09-19 17:58:57 -0500

    code styles only

--- src/server/pages.js
+++ src/server/pages.js
@@ -1,39 +1,34 @@
-const routeMap          = require( '../shared/services/route-map');
-const Router            = require( './router'); 
+const routeMap = require('../shared/services/route-map');
+const Router = require('./router');
 
-const { App }    = require( '../client/components');
-const AppModel   = require( '../shared/models/app' );
+const { App } = require('../client/components');
+const AppModel = require('../shared/models/app');
 
-const { 
-  BODY_REGEX,
-  PATH_TO_INDEX_HTML
-} = require('./config');
+const { BODY_REGEX, PATH_TO_INDEX_HTML } = require('./config');
 
 let router = null;
 
 function renderPage(req, res, next) {
-  router.RenderPath( req.path, req, res, next );
+  router.RenderPath(req.path, req, res, next);
 }
 
 function pagesRoutes(app) {
+  return AppModel.model()
+    .then(appModel => {
+      router = new Router(
+        routeMap.filter(r => !r.browserOnly),
+        { App, appModel },
+        BODY_REGEX,
+        PATH_TO_INDEX_HTML
+      );
 
-  return AppModel.model().then( appModel => {
+      app && app.get('*', renderPage);
 
-    router = new Router( routeMap.filter( r => !r.browserOnly ), 
-                        { App, appModel }, 
-                        BODY_REGEX, 
-                        PATH_TO_INDEX_HTML );
-
-    app && app.get( '*', renderPage );
-
-    console.log( 'Ready for routing'); // eslint-disable-line
-
-  }).catch( err => {
-    
-    console.log( '=====> Error during route initialization ', err ); // eslint-disable-line
-  
-  });
-  
+      console.log('Ready for routing'); // eslint-disable-line
+    })
+    .catch(err => {
+      console.log('=====> Error during route initialization ', err); // eslint-disable-line
+    });
 }
 
 module.exports = pagesRoutes;
--- src/server/static-render.js
+++ src/server/static-render.js
@@ -4,49 +4,54 @@ global.jQuery = function() {};
 
 const fs = require('fs');
 
-const store      = require('../shared/store');
-const routeMap   = require( '../shared/services/route-map');
-const pages      = require('./pages');
-const { 
-  renderHTML,
-  navigateTo 
-} = require('../shared/store/actions/router');
+const store = require('../shared/store');
+const routeMap = require('../shared/services/route-map');
+const pages = require('./pages');
+const { renderHTML, navigateTo } = require('../shared/store/actions/router');
 const { PUBLIC_DIR } = require('./config');
 
-store.subscribe( () => {
+store.subscribe(() => {
+  const {
+    html,
+    route: { path },
+  } = store.getState().router;
 
-  const { html, route: {path} } = store.getState().router;
-
-  if( html ) {
-    writePage( path, html );
+  if (html) {
+    writePage(path, html);
     triggerNextRender();
   }
 });
 
-const routes = routeMap.filter( r => !r.browserOnly );
+const paths = routeMap.filter(el => !el.browserOnly).map(el => el.path);
 let nextPath = 0;
 
 function triggerRender() {
-  store.dispatch( renderHTML(null) );
-  store.dispatch( navigateTo( routes[nextPath++].path, {} ) );
+  store.dispatch(renderHTML(null));
+  store.dispatch(navigateTo(paths[nextPath++], {}));
 }
 
-function triggerNextRender () {
-  if( nextPath !== routes.length ) {
-    process.nextTick( triggerRender );
-  }  
+function triggerNextRender() {
+  if (nextPath !== paths.length) {
+    process.nextTick(triggerRender);
+  }
 }
 
-function writePage(url,html) {
-  let path = url === '/'
-              ? PUBLIC_DIR + '/index.html'
-              : PUBLIC_DIR + url + '/index.html';
-  try { url !== '/' && fs.mkdirSync( PUBLIC_DIR + url ); } catch( e ) { /**/ }              
-  try { fs.unlinkSync( path ); } catch( e ) { /**/ }
-  fs.writeFileSync( path, html );
-  console.log( 'DONE RENDER: ', path );   
+function writePage(url, html) {
+  let path = url === '/' ? PUBLIC_DIR + '/index.html' : PUBLIC_DIR + url + '/index.html';
+  try {
+    url !== '/' && fs.mkdirSync(PUBLIC_DIR + url);
+  } catch (e) {
+    /**/
+  }
+  try {
+    fs.unlinkSync(path);
+  } catch (e) {
+    /**/
+  }
+  fs.writeFileSync(path, html);
+  console.log('DONE RENDER: ', path);
 }
 
-const main = () => pages().then( triggerNextRender );
+const main = () => pages().then(triggerNextRender);
 
 module.exports = main;
